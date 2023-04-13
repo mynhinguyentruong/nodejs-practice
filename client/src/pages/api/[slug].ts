a5762e6cfa41add93a054058b5f2b8a99b8a4b49
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-04-13 11:27:50 -0400

    Add Nextjs as frontend

---
+++ client/src/pages/api/[slug].ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { slug } = req.query
+
+  const data = await fetch(`http://localhost:9000/${slug as string}`)
+  const result = await data.json()
+
+  res.redirect(result)
+}
