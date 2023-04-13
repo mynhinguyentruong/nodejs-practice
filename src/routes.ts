ab55048b2aa94ac4d1f01759882bbe21588499d6
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-04-13 13:34:10 -0400

    Track the link open by logging the user's IP address and other information

--- src/routes.ts
+++ src/routes.ts
@@ -26,10 +26,22 @@ router.put('/url', cors(), (async (_req, res) => {
 }) as RequestHandler)
 
 router.get('/:slug', (async (_req, res) => {
-  const url = await getUrl(_req.params.slug)
+  const { slug } = _req.params
+  const url = await getUrl(slug)
   if (url === null) {
     res.status(501).send('Cannot find the slug')
   }
+
+  // Track the link open by logging the user's IP address and other information
+  const ip = _req.ip
+  const userAgent = _req.headers['user-agent'] ?? ''
+  const time = new Date().toISOString()
+  console.log(`Slug ${slug} opened by ${ip} at ${time} (User Agent: ${userAgent})`);
+
+  // Slug Zu4Y7 opened by ::1 at 2023-04-13T17:29:49.429Z (User Agent: PostmanRuntime/7.30.0) Fetch through POSTMAN
+  // Slug pGfaE opened by ::ffff:127.0.0.1 at 2023-04-13T17:31:49.035Z (User Agent: node-fetch/1.0 (+https://github.com/bitinn/node-fetch)) Fetch through localhost:3002/:slug
+
+
   res.status(200).json(url)
   // res.redirect(302, url as string).json(url)
   // res.status(200).json({ data: url as string })
