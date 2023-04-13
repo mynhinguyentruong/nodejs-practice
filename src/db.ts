6038cc313f0bf345fe168b72fad8b4ac1b8a6fd5
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-04-13 14:03:15 -0400

    Update function createUrl arg

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,14 @@
 import { ShortUrl } from '@prisma/client'
+
 import { prisma } from '../prisma'
 
 // Example: Creating a new ShortUrl
-export async function createUrl(url: string, slug: string): Promise<ShortUrl> {
+export async function createUrl(url: string, slug: string, lastOpened?: Date | null): Promise<ShortUrl> {
   return await prisma.shortUrl.create({
     data: {
       url,
-      slug
+      slug,
+      lastOpened
     }
   })
 }
