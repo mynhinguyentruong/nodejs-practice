1d68184ea718324ed9a543a4c75f6a0a1dd50273
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-04-13 12:43:26 -0400

    Add fetching logic

--- client/src/pages/name.tsx
+++ client/src/pages/name.tsx
@@ -1,9 +1,41 @@
+'use client'
+import { useState } from 'react'
 
 export default function Page() {
 
+  const [url, setUrl] = useState('')
+  console.log(url)
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    console.log(url)
+    let raw = JSON.stringify({
+      "url": url
+    });
+    try {
+      const res = await fetch('http://localhost:9000/url', {
+        method: 'PUT',
+        body: raw,
+        headers: { 'Content-Type': 'application/json' }
+      })
+
+    const data = await res.json()
+    console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  function handleChange(e) {
+    setUrl(e.target.value)
+    console.log(url)
+  }
+
   return (
       <div className="container px-8 py-12 h-screen bg-gray-100 m-z">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-md mx-auto sm:max-w-xl">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-md mx-auto sm:max-w-xl"
+              onSubmit={handleSubmit}
+        >
           <div className="m-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -15,8 +47,9 @@ export default function Page() {
               className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
               type="text"
               id="url"
-              // onChange={}
-              // value={}
+              onChange={handleChange}
+              value={url}
+              name={url}
               placeholder="https://www.google.com"
             />
             <button
